a2405c42791654bb13b9e74ba3338ceed4a438bb
Alex Aguirrebena <alex.aguirrebena@gmail.com>
2020-08-19 14:03:18 -0700

    Added all images to img folder

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import MyNavbar from "./components/navbar/navbar.component";
-import MyCarousal from "./components/my-carousal/my-carousal.component";
-import MyTitleMessage from "./components/title-message/title-message.component";
+import Navbar from "./components/navbar/navbar.component";
+import Carousel from "./components/carousel/carousel.component";
+import Title from "./components/title-message/title-message.component";
 import About from "./pages/about/about.component";
 import Skills from "./pages/skills/skills.component";
 import Container from "react-bootstrap/Container";
@@ -9,7 +9,7 @@ import TimeLine from "./components/projects/projects.component";
 import Experience from "./pages/experience/experience.component";
 import Fade from "react-reveal/Fade";
 import Slide from "react-reveal/Slide";
-import ContactForm from "./pages/contact-form/contact-form.component";
+import ContactForm from "./pages/contact/contact.component";
 import { Parallax } from "react-parallax";
 // import Particles from "react-particles-js";
 // import { particlesOptions } from "./particlesOptions";
@@ -20,9 +20,9 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App" style={{ position: "relative" }}>
-      <MyCarousal />
-      <MyTitleMessage />
-      <MyNavbar />
+      <Carousel/>
+      <Title />
+      <Navbar />
       {/* <Particles
         className="particles particles-box"
         params={particlesOptions}
